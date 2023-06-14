429cf995c3f7e7f73490c5177a69a2099679976a
Oscar Centeno <oscar.centeno@outlook.com>
2023-06-14 18:42:08 +0200

    ci: docker and GH actions

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # js_selenium_dev_demo
 Running samples from Selenium.dev website donde during my personal study. Created from scratch.
+
+
+## Docker
+
+https://github.com/testmoapp/example-github-actions-selenium/tree/main
+
+```bash
+# To start all our four containers, simply change to the dev directory in our project and launch the containers with the
+docker compose up -d
+
+# enter our node container by launching a shell with 
+docker compose exec node bash
+
+# When finished, leave the shell & container by pressing Ctrl+D
+
+# Then from outside the container, you can shut everything down
+$ docker compose down 
--- specs/website/index.js
+++ specs/website/index.js
@@ -19,41 +19,53 @@ class WebBrowser {
             edge: this.startSessionEdge
         }
 
-        await browsers[configs.browser].call(this);
-
+        const selenium_host = process.env.SELENIUM_HOST || 'selenium'
+        const driver = await this.getDriver(selenium_host)
+        await browsers[configs.browser].call(this, driver);
 
         // Setting the implicit wait timeout
         await this.driver.manage().setTimeouts({ implicit: 500 });
     }
 
-    async startSessionChrome() {
+    async getDriver(selenium_host) {
+        const driver = new Builder();
+
+        if (selenium_host === 'localhost') {
+            return driver;
+        }
+        else {
+            return driver.usingServer(`http://${selenium_host}:4444`);
+        }
+    }
+
+    async startSessionChrome(driver) {
         const Chrome = require('selenium-webdriver/chrome');
         const options = new Chrome.Options();
 
         // Creating a new session corresponds with the W3C command for New session
-        this.driver = new Builder()
+        this.driver = await driver
             .forBrowser('chrome')
             .setChromeOptions(options)
             .build();
     }
 
-    async startSessionFirefox() {
+    async startSessionFirefox(driver) {
         const firefox = require('selenium-webdriver/firefox');
         const options = new firefox.Options();
 
-        this.driver = await new Builder()
+        this.driver = await driver
             .forBrowser('firefox')
             .setFirefoxOptions(options)
             .build();
     }
 
-    async startSessionEdge() {
+    async startSessionEdge(driver) {
         const edgedriver = require('@sitespeed.io/edgedriver');
         const edge = require('selenium-webdriver/edge');
 
         // Creating a new session corresponds with the W3C command for New session
         let options = new edge.Options();
-        this.driver = await new Builder()
+        this.driver = await driver
             .forBrowser('MicrosoftEdge')
             .setEdgeOptions(options)
             .setEdgeService(new edge.ServiceBuilder(edgedriver.binPath()))
@@ -86,6 +98,22 @@ class WebBrowser {
     async endSession() {
         await this.driver.quit()
     }
+
+    async takeScreenshot(testName) {
+        const fs = require('fs');
+
+        if (this.driver) {
+            // Take a screenshot of the result page
+            const filename = testName
+                .replace(/['"]+/g, '')
+                .replace(/[^a-z0-9]/gi, '_')
+                .toLowerCase();;
+            const encodedString = await this.driver.takeScreenshot();
+            await fs.writeFileSync(`./screenshots/${filename}.png`,
+                encodedString, 'base64');
+        }
+    }
+
 }
 
 module.exports = WebBrowser;
